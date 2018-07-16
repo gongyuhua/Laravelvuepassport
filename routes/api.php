babd14e94d1e4e44103d2f4f6907835707964397
gongyuhua <yg348@njit.edu>
2018-07-15 20:07:10 -0400

    Write API test.

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::resource('/task', 'TaskController');
-Route::get('/category/{category}/tasks', 'CategoryController@tasks');
-Route::resource('/category', 'CategoryController');
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@register');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('/category/{category}/tasks', 'CategoryController@tasks');
+    Route::resource('/category', 'CategoryController');
+    Route::resource('/task', 'TaskController');
+});
